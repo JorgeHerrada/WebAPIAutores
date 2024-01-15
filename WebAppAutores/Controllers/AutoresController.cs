8f606f97ddee2a7a4aacd612f793f07ee48d5f6f
JLHerradaS <jlherradas@gbm.com.mx>
2024-01-15 11:02:22 -0600

    Lesson 23 - Basic PUT and DELETE

--- WebAppAutores/Controllers/AutoresController.cs
+++ WebAppAutores/Controllers/AutoresController.cs
@@ -35,5 +35,48 @@ namespace WebAppAutores.Controllers
             // HTTP 200 code
             return Ok(); 
         }
+
+        // param gets concatenated to the api route
+        [HttpPut("{id:int}")] // api/autores/{id}
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            // IDs don't match?
+            if (autor.Id != id)
+            {
+                return BadRequest("The author's id does not match the id in the URL");
+            }
+
+            // author with the received ID exist in the DB?
+            var authorExist = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!authorExist)
+            {
+                return NotFound();
+            }
+
+            // prepare the update
+            context.Update(autor);
+
+            // execute the update and save it in DB
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")] // api/autores/{id}
+        public async Task<ActionResult> Delete(int id)
+        {
+            // author with the received ID exist in the DB
+            var authorExist = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!authorExist)
+            {
+                return NotFound();
+            }
+
+            // prepare deletion of the author with the assigned ID
+            context.Remove(new Autor() { Id = id }); ;
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
