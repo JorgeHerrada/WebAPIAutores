91078486f739f8bc451ef782321f2eaa535ff510
JLHerradaS <jlherradas@gbm.com.mx>
2024-01-17 11:42:31 -0600

    Lesson 33 - Default validations in Controller

--- WebAppAutores/Controllers/Entidades/Autor.cs
+++ WebAppAutores/Controllers/Entidades/Autor.cs
@@ -1,9 +1,23 @@
-﻿namespace WebAppAutores.Controllers.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppAutores.Controllers.Entidades
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo '{0}' es requerido")] // Mandatory param and errorMessage
+        [StringLength(maximumLength: 10, ErrorMessage = "El campo {0} no debe de tener más de {1} caracteres")]
         public string Nombre { get; set; }
+        [Range(18,120)]
+        [NotMapped] // no se agrega a la base de datos 
+        public int Edad { get; set; }
+        [CreditCard] // solo valida numeración
+        [NotMapped]
+        public string TarjetaDeCredito { get; set; }
+        [Url]
+        [NotMapped]
+        public string URL { get; set; }
         public List<Libro> Libros { get; set; }
     }
 }
