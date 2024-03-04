dc00b6e662c06fbe29f36de4b0a5c8a4bdbccd49
JorgeHerrada <jorge.herrada.28@gmail.com>
2024-03-04 13:45:15 -0600

    Lesson 103 - Excluding HATEOAS based on Query Params

--- WebAppAutores/Controllers/AutoresController.cs
+++ WebAppAutores/Controllers/AutoresController.cs
@@ -34,7 +34,7 @@ namespace WebAppAutores.Controllers
         [HttpGet(Name = "obtener-autores")] // api/autores -> based on the Route above
         //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)] // protect endpoint
         [AllowAnonymous] // Authentication not needed on this endpoint
-        public async Task<ActionResult<ColeccionDeRecursosDTO<AutorDTO>>> Get() // async MUST return Task<>
+        public async Task<IActionResult> Get([FromQuery] bool incluirHateoas = true) // async MUST return Task<>
         {
             // temp comment, until x.Libros is accessible again
             //return await context.Autores.Include(x => x.Libros).ToListAsync();
@@ -42,29 +42,34 @@ namespace WebAppAutores.Controllers
 
             var dtos = mapper.Map<List<AutorDTO>>(autores);
 
-            var esAdmin = await authorizationService.AuthorizeAsync(User, "esAdmin");
-
-            // iterate the list and generate the links for each author, given its privileges
-            dtos.ForEach(dto => GenerarEnlaces(dto, esAdmin.Succeeded));
-
-            var resultado = new ColeccionDeRecursosDTO<AutorDTO> { Values = dtos };
-
-            resultado.Enlaces.Add(new DatoHATEOSDTO(
-                enlace: Url.Link("obtener-autores", new { }),
-                descripcion: "self",
-                metodo: "GET"
-            ));
-
-            if (esAdmin.Succeeded)
+            if (incluirHateoas)
             {
+                var esAdmin = await authorizationService.AuthorizeAsync(User, "esAdmin");
+
+                // iterate the list and generate the links for each author, given its privileges
+                dtos.ForEach(dto => GenerarEnlaces(dto, esAdmin.Succeeded));
+
+                var resultado = new ColeccionDeRecursosDTO<AutorDTO> { Values = dtos };
+
                 resultado.Enlaces.Add(new DatoHATEOSDTO(
-                    enlace: Url.Link("crear-autor", new { }),
-                    descripcion: "autor-crear",
-                    metodo: "POST"
+                    enlace: Url.Link("obtener-autores", new { }),
+                    descripcion: "self",
+                    metodo: "GET"
                 ));
+
+                if (esAdmin.Succeeded)
+                {
+                    resultado.Enlaces.Add(new DatoHATEOSDTO(
+                        enlace: Url.Link("crear-autor", new { }),
+                        descripcion: "autor-crear",
+                        metodo: "POST"
+                    ));
+                }
+
+                return Ok(resultado);
             }
 
-            return resultado;
+            return Ok(dtos);
         }
 
         // Returns Autor based on ID received
