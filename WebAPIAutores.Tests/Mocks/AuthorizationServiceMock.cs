6eee254e03b1017a7a5bde4b91f8a1806ab138e6
JorgeHerrada <jorge.herrada.28@gmail.com>
2024-03-08 12:25:58 -0600

    Lesson 119 - Mocks

---
+++ WebAPIAutores.Tests/Mocks/AuthorizationServiceMock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebAPIAutores.Tests.Mocks
+{
+    public class AuthorizationServiceMock : IAuthorizationService
+    {
+        public AuthorizationResult DesiredResult { get; set; }
+
+        public Task<AuthorizationResult> AuthorizeAsync(
+            ClaimsPrincipal user, 
+            object? resource, 
+            IEnumerable<IAuthorizationRequirement> requirements)
+        {
+            return Task.FromResult(DesiredResult); // just return success, no real validation
+        }
+
+        public Task<AuthorizationResult> AuthorizeAsync(ClaimsPrincipal user, object? resource, string policyName)
+        {
+            return Task.FromResult(DesiredResult); // just return success, no real validation
+        }
+    }
+}
---
+++ WebAPIAutores.Tests/UnitTests/RootControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using WebAPIAutores.Tests.Mocks;
+using WebAppAutores.Controllers.v1;
+
+namespace WebAPIAutores.Tests.UnitTests
+{
+    [TestClass]
+    public class RootControllerTests
+    {
+        [TestMethod]
+        public async Task When_GetMethodIsCalled_IfUserIsAdmin_ThenAListOfFourItemsIsReturned()
+        {
+            // Arrange
+            var authorizationService = new AuthorizationServiceMock();
+            authorizationService.DesiredResult = AuthorizationResult.Success();
+            var rootController = new RootController(authorizationService);
+            rootController.Url = new URLHelperMock();
+
+            // Act
+            var result = await rootController.Get();
+
+            // Assert
+            Assert.AreEqual(4, result.Value.Count());
+        }
+
+        [TestMethod]
+        public async Task When_GetMethodIsCalled_IfUserIsNotAdmin_ThenAListOfTwoItemsIsReturned()
+        {
+            // Arrange
+            var authorizationService = new AuthorizationServiceMock();
+            authorizationService.DesiredResult = AuthorizationResult.Failed();
+            var rootController = new RootController(authorizationService);
+            rootController.Url = new URLHelperMock();
+
+            // Act
+            var result = await rootController.Get();
+
+            // Assert
+            Assert.AreEqual(2, result.Value.Count());
+        }
+    }
+}
