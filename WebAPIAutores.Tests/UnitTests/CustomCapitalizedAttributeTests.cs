ae7fa75717f058db9c016f9ff831d9b68a0827d1
JorgeHerrada <jorge.herrada.28@gmail.com>
2024-03-08 11:39:37 -0600

    Lesson 118 - Adding additional coverage for CustomCapitalizedAttributeTests

--- WebAPIAutores.Tests/UnitTests/CustomCapitalizedAttributeTests.cs
+++ WebAPIAutores.Tests/UnitTests/CustomCapitalizedAttributeTests.cs
@@ -20,5 +20,35 @@ namespace WebAPIAutores.Tests.UnitTests
             // Assert
             Assert.AreEqual("The field must be Capitalized (Attribute)", result.ErrorMessage);
         }
+        
+        [TestMethod]
+        public void When_ValueIsNull_NoErrorIsThrown()
+        {
+            // Arrange
+            var customCapitalized = new CustomCapitalizedAttribute();
+            string value = null;
+            var valContext = new ValidationContext(new {Nombre = value});
+
+            // Act
+            var result = customCapitalized.GetValidationResult(value, valContext);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void When_ValueIsCapitalized_NoErrorIsThrown()
+        {
+            // Arrange
+            var customCapitalized = new CustomCapitalizedAttribute();
+            var value = "Name";
+            var valContext = new ValidationContext(new {Nombre = value});
+
+            // Act
+            var result = customCapitalized.GetValidationResult(value, valContext);
+
+            // Assert
+            Assert.IsNull(result);
+        }
     }
 }
